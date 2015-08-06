05db19e5509022435ed7b6e2599fb0000da55f17
Dave Longley <dlongley@digitalbazaar.com>
2015-08-06 10:39:01 -0400

    Add default service owner and dev user.

--- bedrock/test.js
+++ bedrock/test.js
@@ -110,4 +110,34 @@ bedrock.config['credentials-mongodb'].provider.credentials.push({
   sysState: 'claimed'
 });
 
+// identities
+config.idp.identities.push({
+  '@context': config.constants.IDENTITY_CONTEXT_V1_URL,
+  type: 'Identity',
+  sysSlug: 'bedrock',
+  label: 'Bedrock',
+  email: 'bedrock@bedrock.dev',
+  sysPassword: 'password',
+  sysResourceRole: [{
+    sysRole: 'identity.registered',
+    generateResource: 'id'
+  }]
+});
+config.idp.identities.push({
+  '@context': config.constants.IDENTITY_CONTEXT_V1_URL,
+  type: 'Identity',
+  sysSlug: 'dev',
+  label: 'Dev',
+  email: 'dev@bedrock.dev',
+  sysPassword: 'password',
+  sysResourceRole: [{
+    sysRole: 'identity.registered',
+    generateResource: 'id'
+  }]
+});
+
+// identity service
+config.idp.owner =
+  config.server.baseUri + config.idp.identityBasePath + '/bedrock';
+
 bedrock.start();

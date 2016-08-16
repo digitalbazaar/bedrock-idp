43a745b9555aedf0129e3be5d9fbe9e6fbb42663
Matthew Collier <collier.matthew@gmail.com>
2016-08-15 21:59:11 -0400

    Add route resolver to check service agreements.

--- components/idp.js
+++ components/idp.js
@@ -5,8 +5,11 @@
  *
  * @author Dave Longley
  */
+/* jshint multistr: true */
+
 define([
   'angular',
+  'lodash',
   './key-view-component',
   './credentials/credentials',
   './dashboard/dashboard',
@@ -14,7 +17,7 @@ define([
   './identity/identity',
   './login/login',
   './passcode/passcode'
-], function(angular, keyViewComponent) {
+], function(angular, _, keyViewComponent) {
 
 'use strict';
 
@@ -24,13 +27,13 @@ var credentialsBasePath =
 var keyBasePath = window.data['bedrock-angular-key'].basePath;
 
 var module = angular.module(
-  'bedrock-idp', Array.prototype.slice.call(arguments, 2).concat([
+  'bedrock-idp', Array.prototype.slice.call(arguments, 3).concat([
     'bedrock.identity', 'bedrock.agreement']));
 
 keyViewComponent(module);
 
 /* @ngInject */
-module.config(function($routeProvider) {
+module.config(function($routeProvider, routeResolverProvider) {
   /* @ngInject */
   function redirectIfReferred(
     $location, $window, brSessionService, config) {
@@ -51,8 +54,75 @@ module.config(function($routeProvider) {
       });
   }
 
+  routeResolverProvider.add('bedrock-idp', 'session', resolve);
+
+  var agreementRedirect = null;
+  /* @ngInject */
+  function resolve($location, $route, brAgreementService, config) {
+    var session = $route.current.locals.session;
+
+    var requiredAgreements =
+      brAgreementService.getAgreements('bedrock-idp.join');
+
+    if(session && session.identity) {
+      // see if identity has signed required agreements
+      var hasRequiredAgreements = _.difference(
+          requiredAgreements, session.identity.agreements).length === 0;
+      if(!hasRequiredAgreements) {
+        if($location.url() === '/agreement') {
+          // already on agreement page, nothing to do
+          return;
+        }
+        // go to agreements page
+        agreementRedirect = $location.url();
+        $location.url('/agreement');
+        throw new Error('Service Agreement required.');
+      }
+
+      // identity has signed required agreements...
+      if($location.url() === '/agreement') {
+        // location is '/agreement' but identity has signed everything, so
+        // go to the dashboard
+        $location.url(config.data.identity.baseUri + '/' +
+          session.identity.sysSlug + '/dashboard');
+        throw new Error('Required agreements already signed.');
+      }
+    }
+  }
+
   var basePath = window.data.idp.identityBasePath;
   $routeProvider
+    .when('/agreement', {
+      title: 'Agreements',
+      session: 'required',
+      template:
+       '<br-agreement-view br-agreement-group="bedrock-idp.join" \
+       br-on-agree="$resolve.relocate()" br-on-cancel="$resolve.cancel()">\
+       </br-agreement-view>',
+      resolve: {
+        cancel: function($route, brSessionService) {
+          return function() {
+            return brSessionService.logout().then(function() {
+              // $location.url('/') does not work here
+              $route.reload();
+            });
+          };
+        },
+        relocate: function($location, $route, config) {
+          return function() {
+            if(agreementRedirect) {
+              var redirect = agreementRedirect;
+              agreementRedirect = null;
+              return $location.url(redirect);
+            }
+            var sysSlug =
+              $route.current.locals.brResolve.session.identity.sysSlug;
+            $location.url(config.data.identity.baseUri + '/' + sysSlug +
+              '/dashboard');
+          };
+        }
+      }
+    })
     .when(basePath + '/:identity/credentials', {
       title: 'Credentials',
       session: 'required',
